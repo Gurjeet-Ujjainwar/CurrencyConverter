708737382bf4f2984ac1ba84730b0b7b82f2b1b0
Gurjeet Ujjainwar <67540881+Gurjeet-Ujjainwar@users.noreply.github.com>
2020-11-26 19:49:16 +0530

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace CurrencyConverter
+{
+    partial class CurrencyConvert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConverter.Location = new System.Drawing.Point(3, 3);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(602, 49);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Currency Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(3, 58);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(602, 263);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // CurrencyConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(612, 333);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnConverter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CurrencyConvert";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "World Currency Converter";
+            this.Load += new System.EventHandler(this.CurrencyConvert_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
+ 
---
+++ CurrencyConverter/CurrencyConverter/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class CurrencyConvert : Form
+    {
+        public CurrencyConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void CurrencyConvert_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.calculator.net/currency-calculator.html");
+        }
+    }
+}
